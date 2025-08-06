0a41b6f3382ee87040596833052c97276a77af8b
Marcos Corrochano Arroyo <mcorrochanoarroyo@gmail.com>
2025-08-06 22:15:14 +0200

    fourth commit

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gamejam.run',
   output: 'server',
-  integrations: [sitemap()]
+  integrations: [sitemap(), preact()]
 })
