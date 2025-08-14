d53585502ee14a203b91b608c7aba7749db96cdd
Marcos <mcorrochanoarroyo@gmail.com>
2025-08-14 15:02:21 +0000

    fifth commit

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
-import preact from '@astrojs/preact'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gamejam.run',
   output: 'server',
-  integrations: [sitemap(), preact()]
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
