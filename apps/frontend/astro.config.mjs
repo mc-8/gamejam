15ecd82a008de2782a887590f749edd1446cb1e3
Marcos Corrochano Arroyo <mcorrochanoarroyo@gmail.com>
2025-08-21 10:46:42 +0200

    add astro-icon

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -2,12 +2,13 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gamejam.run',
   output: 'server',
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()]
   }
